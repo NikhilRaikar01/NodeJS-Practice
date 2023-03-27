cc951b0135eb89f96218b32e839a22c2e8d6bc87
Nikhil Raikar <nikhilraikar@ip-192-168-0-189.ap-south-1.compute.internal>
2023-03-27 22:15:49 +0530

    Final Commit , now starting with npm

--- write_to_file.js
+++ write_to_file.js
@@ -1,5 +1,22 @@
 const fs = require('fs');
+const readline= require('readline');
+
+const writefunc=(name)=>{
 fs.writeFile('greetings.txt', "successfully written into file", (err) => {
     if (err) {console.log("The file has not been saved");}
     else console.log('The file has been saved!');
+  });
+}
+
+const rl=readline.createInterface(
+    {
+        input:process.stdin,
+        output: process.tdout
+
+    }
+);
+
+rl.question('How was your day? ', (name) => {
+    rl.close();
+    writefunc(name);
   });
